dc139515a6b97ca769f27b5fd736e29b5efcac13
Anele Mgcini Mdolomba <AneleMdolomba@gmail.com>
2023-03-22 22:09:38 +0200

    Init skeleton commit

---
+++ src/main/java/co/za/anele/dagacubesplayerapi/DagacubesPlayerApiApplication.java
@@ -0,0 +1,13 @@
+package co.za.anele.dagacubesplayerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagacubesPlayerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DagacubesPlayerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/za/anele/dagacubesplayerapi/DagacubesPlayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.anele.dagacubesplayerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagacubesPlayerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
